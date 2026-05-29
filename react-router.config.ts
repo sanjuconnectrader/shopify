05ff5a1ff20c7410bf94b9a195f6c3d06118aae3
sanjuconnectrader <sanjith@connectrader.io>
2026-05-30 00:38:54 +0530

    commit

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,7 @@
 import type {Config} from '@react-router/dev/config';
-import {vercelPreset} from '@vercel/react-router/vite';
 
 export default {
   appDirectory: 'app',
+  buildDirectory: 'dist',
   ssr: true,
-  presets: [vercelPreset()],
 } satisfies Config;
